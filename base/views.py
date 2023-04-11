fb175422a3f0caa69a2c849303e5358370866c73
Mirza Younus Baig <mirzayounus40@gmail.com>
2023-04-10 23:47:22 -0400

    Configure settings.py, add base app urls.py, and include in root.urls

--- base/views.py
+++ base/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def homeView(request):
+    return HttpResponse('This is the Home Page')
+
+def roomView(request):
+    return HttpResponse('This is the default Room Page')
